826096939bf3bf6fe49166fa66feb4cb69404b47
Christian Letembet <claudechristian.letembetambily@ynov.com>
2018-12-05 14:49:30 +0100

    Migration de la BDD

---
+++ sushiProjet/src/Migrations/Version20181205134840.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181205134840 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE livreur (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(50) NOT NULL, email VARCHAR(60) NOT NULL, telephone VARCHAR(15) NOT NULL, nombre_livraisons INTEGER NOT NULL, temps_livraison VARCHAR(20) NOT NULL, absences INTEGER DEFAULT NULL, etat_commande INTEGER NOT NULL)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE livreur');
+    }
+}
