5b3eb0948f8902d5f12fd3adf13e8e1556e9e361
Christian Letembet <claudechristian.letembetambily@ynov.com>
2018-12-05 14:19:30 +0100

    Dependances Symfony

---
+++ sushiProjet/src/Controller/SushiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SushiController extends AbstractController
+{
+    /**
+     * @Route("/sushi", name="sushi")
+     */
+    public function index()
+    {
+        return $this->render('sushi/index.html.twig', [
+            'controller_name' => 'SushiController',
+        ]);
+    }
+}
